ecfe99bd32f1d34249ad14cc3a9159f04a1456f3
Henrik Jonsson <me@hkjn.me>
2014-09-05 22:34:32 +0200

    makes concrete site type internal, adds Site interface. cuts appengine dependency, adds logging.go

---
+++ logging.go
@@ -0,0 +1,28 @@
+// Logging helpers
+package autosite
+
+import (
+	"net/http"
+)
+
+// LoggerFunc returns a logger from a http request.
+type LoggerFunc func(*http.Request) Logger
+
+// Logger specifies logging functions.
+type Logger interface {
+	// Debugf formats its arguments according to the format, analogous to fmt.Printf,
+	// and records the text as a log message at Debug level.
+	Debugf(format string, args ...interface{})
+
+	// Infof is like Debugf, but at Info level.
+	Infof(format string, args ...interface{})
+
+	// Warningf is like Debugf, but at Warning level.
+	Warningf(format string, args ...interface{})
+
+	// Errorf is like Debugf, but at Error level.
+	Errorf(format string, args ...interface{})
+
+	// Criticalf is like Debugf, but at Critical level.
+	Criticalf(format string, args ...interface{})
+}
